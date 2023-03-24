894b872ca3facb8c0e954d1cb5394cf38586b3a1
luahj2 <119492615+luahj2@users.noreply.github.com>
2023-03-24 15:01:04 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+def cat(update, context):
+    url = "https://api.thecatapi.com/v1/images/search"
+    response = requests.get(url).json()
+    cat_image_url = response[0]['url']
+    update.message.reply_photo(photo=cat_image_url)
+
+def unknown(update, context):
+    update.message.reply_text("I don't know.")
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("cat", cat))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, unknown))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
