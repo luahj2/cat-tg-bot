e74b3cc8128328e2230090dcf48713ba2ab0267c
luahj2 <119492615+luahj2@users.noreply.github.com>
2023-03-25 14:57:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,24 +4,20 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
-def cat(update, context):
+async cat(update, context):
     url = "https://api.thecatapi.com/v1/images/search"
     response = requests.get(url).json()
     cat_image_url = response[0]['url']
-    update.message.reply_photo(photo=cat_image_url)
+    await update.message.reply_photo(photo=cat_image_url)
 
-def unknown(update, context):
-    update.message.reply_text("I don't know.")
+async def unknown(update, context):
+    await update.message.reply_text("I don't know.")
 
 def main():
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("cat", cat))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, unknown))
-
-    updater.start_polling()
-    updater.idle()
+    application = ApplicationBuilder().token(TOKEN).build()
+    application.add_handler(CommandHandler("cat", cat))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, unknown))
+    application.run_polling()
 
 if __name__ == "__main__":
     main()
